d94beea847cd1d7b03df80cadf00e56a5acf8351
Ryuminah <hellorma12@gmail.com>
2022-07-07 02:07:25 +0900

    feat : 타임어택 시스템 추가

---
+++ ProjectRunRun/Assets/Scripts/InGame/System/LifeTimer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeTimer : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("게임 전체 시간")]
+    private float lifeTime;
+
+    private Slider timeSlider;
+    private float currentTime = 0f;
+
+    private void Awake()
+    {
+        timeSlider = GetComponent<Slider>();
+        timeSlider.maxValue = lifeTime;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime <= lifeTime)
+        {
+            currentTime += Time.deltaTime;
+            timeSlider.value = currentTime;
+        }
+
+        else
+        {
+            Debug.Log("Time Over");
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+        
+    }
+}
